43666ea1b77aa838ef1a06f334685c94083b510a
cicerolb <cicerobarasi@gmail.com>
2025-01-25 13:49:59 +0800

    soap map setup

--- Assets/Scripts/RaceManagerScript.cs
+++ Assets/Scripts/RaceManagerScript.cs
@@ -1,11 +1,14 @@
+using System.Collections;
 using System.Threading;
+using TMPro;
 using UnityEngine;
 
 public class RaceManagerScript : MonoBehaviour
 {
     public GameState gameState;
-    public float countDown = 3;
 
+    public GameObject lapCounter1, lapCounter2, lapCounter3;
+    public LapScript lapScript;
     public enum GameState
     {
         PreGame,
@@ -16,13 +19,29 @@ public class RaceManagerScript : MonoBehaviour
     void Start()
     {
         gameState = GameState.PreGame;
-
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (lapScript.lapCounter == 1)
+        {
+            lapCounter1.SetActive(true);
+            lapCounter2.SetActive(false);
+            lapCounter3.SetActive(false);
+        }
+        else if (lapScript.lapCounter == 2)
+        {
+            lapCounter1.SetActive(false);
+            lapCounter2.SetActive(true);
+            lapCounter3.SetActive(false);
+        }
+        else if (lapScript.lapCounter == 3)
+        {
+            lapCounter1.SetActive(false);
+            lapCounter2.SetActive(false);
+            lapCounter3.SetActive(true);
+        }
     }
 
 }
