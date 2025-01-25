757183599865f6729a98783beac683fb42b00e95
cicerolb <cicerobarasi@gmail.com>
2025-01-26 02:30:01 +0800

    fixed lapcollision

--- Assets/LapCollision.cs
+++ Assets/LapCollision.cs
@@ -23,31 +23,28 @@ public class LapCollision : MonoBehaviour
     {
         if (other.CompareTag("Player 1") && player1Verify)
         {
-            if (playerLap.player1Lap < 3)
-            {
-                playerLap.player1Lap++;
-                player1Verify = false;
-            }
+
+            playerLap.player1Lap++;
+            player1Verify = false;
+
 
         }
 
         else if (other.CompareTag("Player 2") && player2Verify)
         {
-            if (playerLap.player2Lap < 3)
-            {
-                playerLap.player2Lap++;
-                player2Verify = false;
-            }
+
+            playerLap.player2Lap++;
+            player2Verify = false;
+
 
         }
 
         else if (other.CompareTag("Player 3") && player3Verify)
         {
-            if (playerLap.player3Lap < 3)
-            {
-                playerLap.player3Lap++;
-                player3Verify = false;
-            }
+
+            playerLap.player3Lap++;
+            player3Verify = false;
+
 
         }
     }
